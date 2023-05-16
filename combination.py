4aa6d5fb29977b55b61e809593de8063239e9709
Amir Hossein <ssiadati@uni-osnabrueck.de>
2023-05-16 20:36:59 +0200

    Commit message

--- combination.py
+++ combination.py
@@ -2,10 +2,19 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combination():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
 
+def combination(arr1, arr2, axis=0):
+    arr1 = np.squeeze(arr1)
+    arr2 = np.squeeze(arr2)
+    arr1_sh=list(arr1.shape)
+    arr2_sh=list(arr2.shape)
+    arr1_sh[axis] = arr2_sh[axis]
+    if arr1_sh != arr2_sh:
+        raise ValueError("Error: Arrays cannot be combined along the given axis")
+    
+    combined_arr = np.concatenate((arr1, arr2), axis=axis)
+    
+    return combined_arr
 
 if __name__ == "__main__":
     # use this for your own testing!
--- gaussian_analysis.py
+++ gaussian_analysis.py
@@ -2,10 +2,23 @@ import numpy as np
 
 # implement the function gaussian_analysis
 
-def gaussian_analysis():
-        
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def gaussian_analysis(loc, scale, lower_bound, upper_bound):
+
+    if not all(isinstance(x, (int, float)) for x in [loc, scale, lower_bound, upper_bound]):
+        return "Error: All inputs must be integers or floats"
+    
+    if lower_bound >= upper_bound:
+        return "Error: lower_bound must be smaller than upper_bound"
+    
+    arr = np.random.normal(loc=loc, scale=scale, size=100)
+    
+    arr = arr[(arr >= lower_bound) & (arr <= upper_bound)]
+    
+    mean = np.mean(arr)
+    std = np.std(arr)
+    
+    return (mean, std)
+
 
 
 if __name__ == "__main__":
